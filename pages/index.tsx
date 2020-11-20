87c6d08cb94877e98583195a4231b0007f888279
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-11-20 09:05:10 -0600

    gql inc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import { Fragment } from 'react';
+import { Meta, Navbar } from '@components/index';
+import { NextPage } from 'next';
+import Head from 'next/head';
 
-const Index = () => {
+const Index = ({}: NextPage) => {
 	return (
 		<Fragment>
-			<div>INIT</div>
+			<Meta />
+			<Head>
+				<title>Mark W Jacob Dental Group</title>
+			</Head>
+			<div className='flex mx-5 min-w-full w-full'>
+				<Navbar className='min-w-full w-full -mx-5 font-poppins' />
+			</div>
 		</Fragment>
 	);
 };
