f3fd3da74aa541cdeff003029fbd4477a46875d9
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-06 12:22:24 -0600

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true'
-});
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 const withImages = {
 	images: {
 		domains: ['drisdell.com', 'secure.gravatar.com']
@@ -29,8 +27,28 @@ const webpackBundle = {
 	}
 };
 
+const nextConfig = {
+	analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+	analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+	bundleAnalyzerConfig: {
+		server: {
+			analyzerMode: 'static',
+			reportFilename: '../bundles/server.html'
+		},
+		browser: {
+			analyzerMode: 'static',
+			reportFilename: '../bundles/client.html'
+		}
+	}
+};
+
 module.exports = withPlugins([
-	[withBundleAnalyzer({})],
+	[
+		withBundleAnalyzer({
+			enabled: process.env.ANALYZE === 'true'
+		})
+	],
+	nextConfig,
 	withImages,
 	webpackBundle
 ]);
