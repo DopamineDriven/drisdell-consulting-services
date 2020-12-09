087b375a31c9866f22df263c741f8015e8a18f9f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-09 09:57:43 -0600

    consultant well underway

--- components/Consultants/Children/index.ts
+++ components/Consultants/Children/index.ts
@@ -1,3 +1,5 @@
 export { default as ConsultantContent } from './ConsultantContent';
 export { default as ConsultantCover } from './ConsultantCover';
+export { default as ConsultantLayout } from './ConsultantLayout';
 export { default as ConsultantTitle } from './ConsultantTitle';
+export { default } from './ConsultantWrapper';
