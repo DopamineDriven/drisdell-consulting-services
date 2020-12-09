3eaa7effaa3bbdedb8071c79ef0421b81b982cde
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-09 04:28:32 -0600

    about-wrapper FC with React.ReactNode props as children for coalescence incorporated

---
+++ components/About/Children/AboutParent/about-parent.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react';
+// import cn from classNames;
+import { AboutPage } from '@lib/graphql/AboutPage/__generated__/AboutPage';
+import AboutLayout from '../AboutLayout';
+
+interface AboutParentProps {
+	data: AboutPage;
+	children?: React.ReactNode;
+	error: undefined;
+}
+
+const AboutParent: FC<AboutParentProps> = props => {
+	const { data, children, error } = props;
+
+	return (
+		<>
+			{data != null &&
+			data.pages != null &&
+			data.pages.edges != null &&
+			data.pages.edges.length > 0 ? (
+				data.pages.edges.map(page => {
+					return page != null &&
+						page.node != null &&
+						page.node.featuredImage != null &&
+						page.node.children != null ? (
+						<div
+							className='block mx-auto font-poppins w-full max-w-cardGrid'
+							key={page.node.__typename}
+						>
+							<AboutLayout
+								title={page.node.title}
+								children={page.node.children}
+								content={page.node.content}
+								featuredImage={page.node.featuredImage}
+							/>
+							{children}
+						</div>
+					) : (
+						<div>error on about page mapping</div>
+					);
+				})
+			) : (
+				<div>an error occurred...{error}</div>
+			)}
+		</>
+	);
+};
+
+export default AboutParent;
--- components/About/Children/index.ts
+++ components/About/Children/index.ts
@@ -1,2 +1,3 @@
 export { default as AboutLayout } from './AboutLayout';
+export { default as AboutParent } from './AboutParent';
 export { default } from './AboutWrapper';
