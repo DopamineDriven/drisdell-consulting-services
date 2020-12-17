9d78d409311e4ad4b3f27773001a443d57a9a198
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-17 12:56:48 -0600

    updates

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,9 @@
 import { Fragment } from 'react';
 import { Layout } from '@components/index';
 import { PageTitle } from '@lib/index';
+import { NextPage } from 'next';
 
-const Contact = () => {
+const Contact: NextPage = () => {
 	const { CONTACT } = PageTitle;
 	return (
 		<Fragment>
