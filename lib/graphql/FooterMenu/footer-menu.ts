86bc454789df9cde0fef9c398d04d8628f2f5f0f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-17 04:48:19 -0600

    resolved fragment conundrum

--- lib/graphql/FooterMenu/footer-menu.ts
+++ lib/graphql/FooterMenu/footer-menu.ts
@@ -2,11 +2,12 @@ import { gql } from '@apollo/client';
 import { FRAGMENT_MENU_FIELDS } from '@lib/graphql';
 
 const FOOTER_MENU = gql`
+	${FRAGMENT_MENU_FIELDS}
 	query FooterMenu {
 		footerMenu: menuItems(where: { location: FOOTER, parentId: "0" }) {
 			edges {
 				node {
-					...${FRAGMENT_MENU_FIELDS}
+					...MenuFragment
 				}
 			}
 		}
