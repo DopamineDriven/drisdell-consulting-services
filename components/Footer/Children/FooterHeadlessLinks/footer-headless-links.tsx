b4687d849a1392e1a1b1be04d49da3e41f1e5426
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-12-25 16:46:54 -0600

    updates

---
+++ components/Footer/Children/FooterHeadlessLinks/footer-headless-links.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import cn from 'classnames';
+import css from './footer-nav-links-headless.module.css';
+import { HeaderFooter_footerDynamic_menuItems_edges_node as FooterRef } from '@lib/graphql/HeaderFooter/__generated__/HeaderFooter';
+
+export interface FooterLinkPropsHeadless extends FooterRef {
+	root?: string;
+}
+
+const FooterNavLinksHeadless: FC<FooterLinkPropsHeadless> = props => {
+	const { root, path, label } = props;
+	const { pathname } = useRouter();
+
+	return (
+		<li className={css.footerLinksLi}>
+			<Link href={path} passHref>
+				<a
+					className={
+						pathname === path ? cn(css.linkActive, root) : cn(css.links, root)
+					}
+					aria-label={`link to ${label!}`}
+				>
+					{label ?? ''}
+				</a>
+			</Link>
+		</li>
+	);
+};
+
+export default FooterNavLinksHeadless;
