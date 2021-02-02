10b737d1b1b3fc859afb35d7df1255ab4732beba
Andrew Ross <andrew.simpson.ross@gmail.com>
2021-02-02 00:48:53 -0600

    updates

--- lib/artsy-fresnel.ts
+++ lib/artsy-fresnel.ts
@@ -2,11 +2,13 @@ import { createMedia } from '@artsy/fresnel/dist/index';
 
 const PortfolioMedia = createMedia({
 	breakpoints: {
-		xs: 0,
+		xs: 375,
 		sm: 640,
 		md: 768,
 		lg: 1024,
-		xl: 1280
+		xl: 1280,
+		'2xl': 1440,
+		'3xl': 1920
 	}
 });
 
